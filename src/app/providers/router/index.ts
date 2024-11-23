4e11c13b182365ab290c685bb23593ca64e27839
Natasha <71723641+ranatasha@users.noreply.github.com>
2024-11-23 20:09:14 +0300

    minor fix

--- src/app/providers/router/index.ts
+++ src/app/providers/router/index.ts
@@ -4,13 +4,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/choose_folder',
+      path: '/',
       name: 'chooseFolder',
       component: () => import('@pages/ChooseFolderPage.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      redirect: { name: 'chooseFolder'},
     },
   ],
 });
